a7b9ad69204cd0e2fecb5c0af500d0de183b1cb6
sieun <charm516@naver.com>
2024-09-23 20:38:48 +0900

    [refactoring] - axios로 변경

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,14 +1,16 @@
 import TodoList from "../components/todo/TodoList";
 import TodoForm from "../components/todo/TodoForm";
 import { Todo } from "@/app/types/todo.type";
+import axios from "axios";
 
 const TodoPage = async () => {
-  const response = await fetch("http://localhost:4000/todos", {
-    next: {
-      revalidate: 5, // 5초마다 새로운 데이터 가져옴
+  const response = await axios.get("http://localhost:4000/todos", {
+    headers: {
+      "Cache-Control": "no-cache", // 캐시를 사용하지 않도록 설정
     },
   });
-  const todos: Todo[] = await response.json();
+
+  const todos: Todo[] = response.data;
 
   const inProgressTodo = todos.filter((todo) => !todo.isDone);
   const doneTodo = todos.filter((todo) => todo.isDone);
@@ -21,5 +23,8 @@ const TodoPage = async () => {
     </>
   );
 };
+
+// 페이지 컴포넌트에서 revalidate 설정
+export const revalidate = 5; // 5초마다 새로운 데이터 가져옴
 
 export default TodoPage;
