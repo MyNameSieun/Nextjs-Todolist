5d864ab838c61a5604b0ce1403f14f4685b11d88
박시은 <charm516@naver.com>
2024-09-24 01:22:47 +0900

    [docs] - font 제거

--- 03_zustand/app/layout.tsx
+++ 03_zustand/app/layout.tsx
@@ -1,16 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +11,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="ko">
+      <body>{children}</body>
     </html>
   );
 }
---
+++ README.md
@@ -0,0 +1,46 @@
+`NEXT TODOLIST`
+
+## 1. props-driiling
+
+> 설치한 라이브러리: x
+
+## 2. json-server
+
+> 설치한 라이브러리: `axios`, `json-server`
+
+## 3. Zustand----->
+
+> 설치한 라이브러리: `zustand`
+
+- 전역 상태 관리로만 사용(서버 통신x),
+- todo 디테일 페이지 적용
+
+```
+C:.
+│   favicon.ico
+│   layout.tsx
+│   page.tsx
+│
+├───assets
+│       sample-todo.ts
+│
+├───components
+│   └───todo
+│           TodoForm.tsx
+│           TodoItem.tsx
+│           TodoList.tsx
+│
+├───details
+│   └───[todoId]
+│           page.tsx
+│
+├───store
+│       todo-store.ts
+│
+└───types
+        todo-types.ts
+```
+
+## 4. react-query
+
+> 설치한 라이브러리: `axios`, `json-server`, `react-query`
